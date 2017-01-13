712b262816a5d171f67e773137beca879f84124a
Vladimir <nekrasowladimir@gmail.com>
2017-01-13 14:12:06 +0900

    Create fine_tune.py
    Script for fine-tuning the init model.

---
+++ fine_tune.py
@@ -0,0 +1,198 @@
+"""Training script for the DeepLab-ResNet network on the PASCAL VOC dataset
+   for semantic image segmentation.
+
+This script fine-tunes the model using augmented PASCAL VOC,
+which contains approximately 10000 images for training and 1500 images for validation.
+Only the last 'fc1_voc12' layers are being trained.
+"""
+
+from __future__ import print_function
+
+import argparse
+from datetime import datetime
+import os
+import sys
+import time
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+import numpy as np
+
+from deeplab_resnet import DeepLabResNetModel, ImageReader, decode_labels, prepare_label
+
+n_classes = 21
+
+BATCH_SIZE = 4
+DATA_DIRECTORY = '/home/VOCdevkit'
+DATA_LIST_PATH = './dataset/train.txt'
+INPUT_SIZE = '321,321'
+LEARNING_RATE = 1e-4
+NUM_STEPS = 20000
+RANDOM_SCALE = True
+RESTORE_FROM = './deeplab_resnet.ckpt'
+SAVE_DIR = './images_finetune/'
+SAVE_NUM_IMAGES = 2
+SAVE_PRED_EVERY = 100
+SNAPSHOT_DIR = './snapshots_finetune/'
+
+IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
+
+def get_arguments():
+    """Parse all the arguments provided from the CLI.
+    
+    Returns:
+      A list of parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="DeepLab-ResNet Network")
+    parser.add_argument("--batch_size", type=int, default=BATCH_SIZE,
+                        help="Number of images sent to the network in one step.")
+    parser.add_argument("--data_dir", type=str, default=DATA_DIRECTORY,
+                        help="Path to the directory containing the PASCAL VOC dataset.")
+    parser.add_argument("--data_list", type=str, default=DATA_LIST_PATH,
+                        help="Path to the file listing the images in the dataset.")
+    parser.add_argument("--input_size", type=str, default=INPUT_SIZE,
+                        help="Comma-separated string with height and width of images.")
+    parser.add_argument("--learning_rate", type=float, default=LEARNING_RATE,
+                        help="Learning rate for training.")
+    parser.add_argument("--num_steps", type=int, default=NUM_STEPS,
+                        help="Number of training steps.")
+    parser.add_argument("--restore_from", type=str, default=RESTORE_FROM,
+                        help="Where restore model parameters from.")
+    parser.add_argument("--save_dir", type=str, default=SAVE_DIR,
+                        help="Where to save figures with predictions.")
+    parser.add_argument("--save_num_images", type=int, default=SAVE_NUM_IMAGES,
+                        help="How many images to save.")
+    parser.add_argument("--save_pred_every", type=int, default=SAVE_PRED_EVERY,
+                        help="Save figure with predictions and ground truth every often.")
+    parser.add_argument("--snapshot_dir", type=str, default=SNAPSHOT_DIR,
+                        help="Where to save snapshots of the model.")
+    return parser.parse_args()
+
+def save(saver, sess, logdir, step):
+    model_name = 'model.ckpt'
+    checkpoint_path = os.path.join(logdir, model_name)
+    
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+
+    saver.save(sess, checkpoint_path, global_step=step)
+    print('The checkpoint has been created.')
+
+def load(saver, sess, ckpt_path):
+    '''Load trained weights.
+    
+    Args:
+      saver: TensorFlow saver object.
+      sess: TensorFlow session.
+      ckpt_path: path to checkpoint file with parameters.
+    ''' 
+    saver.restore(sess, ckpt_path)
+    print("Restored model parameters from {}".format(ckpt_path))
+
+def main():
+    """Create the model and start the training."""
+    args = get_arguments()
+    
+    h, w = map(int, args.input_size.split(','))
+    input_size = (h, w)
+    
+    # Create queue coordinator.
+    coord = tf.train.Coordinator()
+    
+    # Load reader.
+    with tf.name_scope("create_inputs"):
+        reader = ImageReader(
+            args.data_dir,
+            args.data_list,
+            input_size,
+            RANDOM_SCALE,
+            coord)
+        image_batch, label_batch = reader.dequeue(args.batch_size)
+    
+    # Create network.
+    net = DeepLabResNetModel({'data': image_batch}, is_training=False)
+    # For a small batch size, it is better to keep 
+    # the statistics of the BN layers (running means and variances)
+    # frozen, and to not update the values provided by the pre-trained model. 
+    # If is_training=True, the statistics will be updated during the training.
+    # Note that is_training=False still updates BN parameters gamma (scale) and beta (offset)
+    # if they are presented in var_list of the optimiser definition.
+
+    # Predictions.
+    raw_output = net.layers['fc1_voc12']
+    # Which variables to load. Running means and variances are not trainable,
+    # thus all_variables() should be restored.
+    restore_var = tf.all_variables()
+    
+    
+    prediction = tf.reshape(raw_output, [-1, n_classes])
+    label_proc = prepare_label(label_batch, tf.pack(raw_output.get_shape()[1:3]))
+    gt = tf.reshape(label_proc, [-1, n_classes])
+    
+    # Pixel-wise softmax loss.
+    loss = tf.nn.softmax_cross_entropy_with_logits(prediction, gt)
+    reduced_loss = tf.reduce_mean(loss)
+    
+    # Processed predictions.
+    raw_output_up = tf.image.resize_bilinear(raw_output, tf.shape(image_batch)[1:3,])
+    raw_output_up = tf.argmax(raw_output_up, dimension=3)
+    pred = tf.expand_dims(raw_output_up, dim=3)
+    
+    # Define loss and optimisation parameters.
+    optimiser = tf.train.AdamOptimizer(learning_rate=args.learning_rate)
+    trainable = [v for v in tf.trainable_variables() if 'fc1_voc12' in v.name] # Fine-tune only the last layers.
+    optim = optimiser.minimize(reduced_loss, var_list=trainable)
+    
+    # Set up tf session and initialize variables. 
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    init = tf.initialize_all_variables()
+    
+    sess.run(init)
+    
+    # Saver for storing checkpoints of the model.
+    saver = tf.train.Saver(var_list=restore_var, max_to_keep=40)
+    
+    # Load variables if the checkpoint is provided.
+    if args.restore_from is not None:
+        loader = tf.train.Saver(var_list=restore_var)
+        load(loader, sess, args.restore_from)
+    
+    # Start queue threads.
+    threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+        
+    # Iterate over training steps.
+    for step in range(args.num_steps):
+        start_time = time.time()
+        
+        if step % args.save_pred_every == 0:
+            loss_value, images, labels, preds, _ = sess.run([reduced_loss, image_batch, label_batch, pred, optim])
+            fig, axes = plt.subplots(args.save_num_images, 3, figsize = (16, 12))
+            for i in xrange(args.save_num_images):
+                axes.flat[i * 3].set_title('data')
+                axes.flat[i * 3].imshow((images[i] + IMG_MEAN)[:, :, ::-1].astype(np.uint8))
+
+                axes.flat[i * 3 + 1].set_title('mask')
+                axes.flat[i * 3 + 1].imshow(decode_labels(labels[i, :, :, 0]))
+
+                axes.flat[i * 3 + 2].set_title('pred')
+                axes.flat[i * 3 + 2].imshow(decode_labels(preds[i, :, :, 0]))
+            plt.savefig(args.save_dir + str(start_time) + ".png")
+            plt.close(fig)
+            save(saver, sess, args.snapshot_dir, step)
+        else:
+            loss_value, _ = sess.run([reduced_loss, optim])
+        duration = time.time() - start_time
+        print('step {:d} \t loss = {:.3f}, ({:.3f} sec/step)'.format(step, loss_value, duration))
+    coord.request_stop()
+    coord.join(threads)
+    
+if __name__ == '__main__':
+    main()
