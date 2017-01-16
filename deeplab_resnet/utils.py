2e13fedccd94b7e9e45f4e9b23795902a470a26b
Vladimir <nekrasowladimir@gmail.com>
2017-01-16 17:27:22 +0900

    Update utils.py
    Added inverse pre-processing transformation of the input images.

--- deeplab_resnet/utils.py
+++ deeplab_resnet/utils.py
@@ -14,6 +14,8 @@ label_colours = [(0,0,0)
                 # 11=diningtable, 12=dog, 13=horse, 14=motorbike, 15=person
                 ,(0,64,0),(128,64,0),(0,192,0),(128,192,0),(0,64,128)]
                 # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+# image mean
+IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
     
 def decode_labels(mask, num_images):
     """Decode batch of segmentation masks.
@@ -55,3 +57,20 @@ def prepare_label(input_batch, new_size):
         input_batch = tf.one_hot(input_batch, depth=n_classes)
     return input_batch
 
+def inv_preprocess(imgs, num_images):
+  """Inverse preprocessing of the batch of images.
+     Add the mean vector and convert from BGR to RGB.
+   
+  Args:
+    imgs: batch of input images.
+    num_images: number of images to apply the inverse transformations on.
+  
+  Returns:
+    The batch of the size num_images with the same spatial dimensions as the input.
+  """
+  n, h, w, c = imgs.shape
+  assert(n >= num_images, 'Batch size %d should be greater or equal than number of images to save %d.' % (n, num_images))
+  outputs = np.zeros((num_images, h, w, c), dtype=np.uint8)
+  for i in range(num_images):
+    outputs[i] = (imgs[i] + IMG_MEAN)[:, :, ::-1].astype(np.uint8)
+  return outputs
