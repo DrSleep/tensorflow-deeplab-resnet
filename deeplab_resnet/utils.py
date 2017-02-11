ff035fd52db66bc2b9f33b072a05aa18fa0a763b
Vladimir <nekrasowladimir@gmail.com>
2017-02-11 11:15:03 +0900

    Update utils.py

--- deeplab_resnet/utils.py
+++ deeplab_resnet/utils.py
@@ -40,7 +40,7 @@ def decode_labels(mask, num_images=1):
       outputs[i] = np.array(img)
     return outputs
 
-def prepare_label(input_batch, new_size):
+def prepare_label(input_batch, new_size, one_hot=True):
     """Resize masks and perform one-hot encoding.
 
     Args:
@@ -54,7 +54,8 @@ def prepare_label(input_batch, new_size):
     with tf.name_scope('label_encode'):
         input_batch = tf.image.resize_nearest_neighbor(input_batch, new_size) # as labels are integer numbers, need to use NN interp.
         input_batch = tf.squeeze(input_batch, squeeze_dims=[3]) # reducing the channel dimension.
-        input_batch = tf.one_hot(input_batch, depth=n_classes)
+        if one_hot:
+          input_batch = tf.one_hot(input_batch, depth=n_classes)
     return input_batch
 
 def inv_preprocess(imgs, num_images):
