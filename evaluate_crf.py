c386b6f5a881630cee3bfcc8f24c039734661524
naldeborgh7575 <haaldeborgh@gmail.com>
2017-09-08 13:06:33 -0600

    add crf from branch, update to tf r1.1

---
+++ evaluate_crf.py
@@ -0,0 +1,130 @@
+"""Evaluation script for the DeepLab-ResNet network on the validation subset
+   of PASCAL VOC dataset.
+
+This script evaluates the model on 1449 validation images.
+"""
+
+from __future__ import print_function
+
+import argparse
+from datetime import datetime
+import os
+import sys
+import time
+
+import tensorflow as tf
+import numpy as np
+from tqdm import tqdm
+
+from deeplab_resnet import DeepLabResNetModel, ImageReader, dense_crf, inv_preprocess, prepare_label
+
+IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
+
+DATA_DIRECTORY = '/home/VOCdevkit'
+DATA_LIST_PATH = './dataset/val.txt'
+IGNORE_LABEL = 255
+NUM_CLASSES = 21
+NUM_STEPS = 1449 # Number of images in the validation set.
+RESTORE_FROM = './deeplab_resnet.ckpt'
+
+def get_arguments():
+    """Parse all the arguments provided from the CLI.
+
+    Returns:
+      A list of parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="DeepLabLFOV Network")
+    parser.add_argument("--data-dir", type=str, default=DATA_DIRECTORY,
+                        help="Path to the directory containing the PASCAL VOC dataset.")
+    parser.add_argument("--data-list", type=str, default=DATA_LIST_PATH,
+                        help="Path to the file listing the images in the dataset.")
+    parser.add_argument("--ignore-label", type=int, default=IGNORE_LABEL,
+                        help="The index of the label to ignore during the training.")
+    parser.add_argument("--num-classes", type=int, default=NUM_CLASSES,
+                        help="Number of classes to predict (including background).")
+    parser.add_argument("--num-steps", type=int, default=NUM_STEPS,
+                        help="Number of images in the validation set.")
+    parser.add_argument("--restore-from", type=str, default=RESTORE_FROM,
+                        help="Where restore model parameters from.")
+    return parser.parse_args()
+
+def load(saver, sess, ckpt_path):
+    '''Load trained weights.
+
+    Args:
+      saver: TensorFlow saver object.
+      sess: TensorFlow session.
+      ckpt_path: path to checkpoint file with parameters.
+    '''
+    saver.restore(sess, ckpt_path)
+    print("Restored model parameters from {}".format(ckpt_path))
+
+def main():
+    """Create the model and start the evaluation process."""
+    args = get_arguments()
+
+    # Create queue coordinator.
+    coord = tf.train.Coordinator()
+
+    # Load reader.
+    with tf.name_scope("create_inputs"):
+        reader = ImageReader(
+            args.data_dir,
+            args.data_list,
+            None, # No defined input size.
+            False, # No random scale.
+            False, # No random mirror.
+            args.ignore_label,
+            IMG_MEAN,
+            coord)
+        image, label = reader.image, reader.label
+    image_batch, label_batch = tf.expand_dims(image, dim=0), tf.expand_dims(label, dim=0) # Add one batch dimension.
+
+    # Create network.
+    net = DeepLabResNetModel({'data': image_batch}, is_training=False, num_classes=args.num_classes)
+
+    # Which variables to load.
+    restore_var = tf.global_variables()
+
+    # Predictions.
+    raw_output = net.layers['fc1_voc12']
+    raw_output = tf.image.resize_bilinear(raw_output, tf.shape(image_batch)[1:3,])
+    raw_output = tf.argmax(raw_output, dimension=3)
+    pred = tf.expand_dims(raw_output, dim=3) # Create 4-d tensor.
+
+    # CRF.
+    inv_image = tf.py_func(inv_preprocess, [image_batch], tf.uint8)
+    raw_output = tf.py_func(dense_crf, [tf.nn.softmax(raw_output), inv_image], tf.float32)
+
+    # mIoU
+    pred = tf.reshape(pred, [-1,])
+    gt = tf.reshape(label_batch, [-1,])
+    weights = tf.cast(tf.less_equal(gt, args.num_classes - 1), tf.int32) # Ignoring all labels greater than or equal to n_classes.
+    mIoU, update_op = tf.contrib.metrics.streaming_mean_iou(pred, gt, num_classes=args.num_classes, weights=weights)
+
+    # Set up tf session and initialize variables.
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    init = tf.global_variables_initializer()
+
+    sess.run(init)
+    sess.run(tf.local_variables_initializer())
+
+    # Load weights.
+    loader = tf.train.Saver(var_list=restore_var)
+    if args.restore_from is not None:
+        load(loader, sess, args.restore_from)
+
+    # Start queue threads.
+    threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+
+    # Iterate over training steps.
+    for step in tqdm(range(args.num_steps)):
+        preds, _ = sess.run([pred, update_op])
+    print('Mean IoU: {:.3f}'.format(mIoU.eval(session=sess)))
+    coord.request_stop()
+    coord.join(threads)
+
+if __name__ == '__main__':
+    main()
---
+++ inference_crf.py
@@ -0,0 +1,109 @@
+"""Run DeepLab-ResNet on a given image.
+
+This script computes a segmentation mask for a given image.
+"""
+
+from __future__ import print_function
+
+import argparse
+from datetime import datetime
+import os
+import sys
+import time
+
+from PIL import Image
+
+import tensorflow as tf
+import numpy as np
+
+from deeplab_resnet import DeepLabResNetModel, ImageReader, decode_labels, dense_crf, prepare_label
+
+IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
+
+NUM_CLASSES = 21
+SAVE_DIR = './output/'
+
+def get_arguments():
+    """Parse all the arguments provided from the CLI.
+
+    Returns:
+      A list of parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="DeepLabLFOV Network Inference.")
+    parser.add_argument("img_path", type=str,
+                        help="Path to the RGB image file.")
+    parser.add_argument("model_weights", type=str,
+                        help="Path to the file with model weights.")
+    parser.add_argument("--num-classes", type=int, default=NUM_CLASSES,
+                        help="Number of classes to predict (including background).")
+    parser.add_argument("--save-dir", type=str, default=SAVE_DIR,
+                        help="Where to save predicted mask.")
+    return parser.parse_args()
+
+def load(saver, sess, ckpt_path):
+    '''Load trained weights.
+
+    Args:
+      saver: TensorFlow saver object.
+      sess: TensorFlow session.
+      ckpt_path: path to checkpoint file with parameters.
+    '''
+    saver.restore(sess, ckpt_path)
+    print("Restored model parameters from {}".format(ckpt_path))
+
+def main():
+    """Create the model and start the evaluation process."""
+    args = get_arguments()
+
+    # Prepare image.
+    img_orig = tf.image.decode_jpeg(tf.read_file(args.img_path), channels=3)
+    # Convert RGB to BGR.
+    img_r, img_g, img_b = tf.split(axis=2, num_or_size_splits=3, value=img_orig)
+    img = tf.cast(tf.concat(axis=2, values=[img_b, img_g, img_r]), dtype=tf.float32)
+    # Extract mean.
+    img -= IMG_MEAN
+
+    # Create network.
+    net = DeepLabResNetModel({'data': tf.expand_dims(img, dim=0)}, is_training=False, num_classes=args.num_classes)
+
+    # Which variables to load.
+    restore_var = tf.global_variables()
+
+    # Predictions.
+    raw_output = net.layers['fc1_voc12']
+    raw_output_up = tf.image.resize_bilinear(raw_output, tf.shape(img)[0:2,])
+
+    # CRF.
+    raw_output_up = tf.nn.softmax(raw_output_up)
+    raw_output_up = tf.py_func(dense_crf, [raw_output_up, tf.expand_dims(img_orig, dim=0)], tf.float32)
+
+    raw_output_up = tf.argmax(raw_output_up, dimension=3)
+    pred = tf.expand_dims(raw_output_up, dim=3)
+
+
+    # Set up TF session and initialize variables.
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    init = tf.global_variables_initializer()
+
+    sess.run(init)
+
+    # Load weights.
+    loader = tf.train.Saver(var_list=restore_var)
+    load(loader, sess, args.model_weights)
+
+    # Perform inference.
+    preds = sess.run(pred)
+
+    msk = decode_labels(preds, num_classes=args.num_classes)
+    im = Image.fromarray(msk[0])
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+    im.save(args.save_dir + 'mask.png')
+
+    print('The output file has been saved to {}'.format(args.save_dir + 'mask.png'))
+
+
+if __name__ == '__main__':
+    main()
