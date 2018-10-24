9de50f0a1142994f936363283c7b781983349293
James W. McClain <jmcclain@azavea.com>
2018-10-24 19:06:46 -0400

    Remove Excess Whitespace

--- inference.py
+++ inference.py
@@ -19,13 +19,13 @@ import numpy as np
 from deeplab_resnet import DeepLabResNetModel, ImageReader, decode_labels, prepare_label
 
 IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
-    
+
 NUM_CLASSES = 21
 SAVE_DIR = './output/'
 
 def get_arguments():
     """Parse all the arguments provided from the CLI.
-    
+
     Returns:
       A list of parsed arguments.
     """
@@ -42,27 +42,27 @@ def get_arguments():
 
 def load(saver, sess, ckpt_path):
     '''Load trained weights.
-    
+
     Args:
       saver: TensorFlow saver object.
       sess: TensorFlow session.
       ckpt_path: path to checkpoint file with parameters.
-    ''' 
+    '''
     saver.restore(sess, ckpt_path)
     print("Restored model parameters from {}".format(ckpt_path))
 
 def main():
     """Create the model and start the evaluation process."""
     args = get_arguments()
-    
+
     # Prepare image.
     img = tf.image.decode_jpeg(tf.read_file(args.img_path), channels=3)
     # Convert RGB to BGR.
     img_r, img_g, img_b = tf.split(axis=2, num_or_size_splits=3, value=img)
     img = tf.cast(tf.concat(axis=2, values=[img_b, img_g, img_r]), dtype=tf.float32)
     # Extract mean.
-    img -= IMG_MEAN 
-    
+    img -= IMG_MEAN
+
     # Create network.
     net = DeepLabResNetModel({'data': tf.expand_dims(img, dim=0)}, is_training=False, num_classes=args.num_classes)
 
@@ -75,22 +75,22 @@ def main():
     raw_output_up = tf.argmax(raw_output_up, dimension=3)
     pred = tf.expand_dims(raw_output_up, dim=3)
 
-    
-    # Set up TF session and initialize variables. 
+
+    # Set up TF session and initialize variables.
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
     init = tf.global_variables_initializer()
-    
+
     sess.run(init)
-    
+
     # Load weights.
     loader = tf.train.Saver(var_list=restore_var)
     load(loader, sess, args.model_weights)
-    
+
     # Perform inference.
     preds = sess.run(pred)
-    
+
     msk = decode_labels(preds, num_classes=args.num_classes)
     im = Image.fromarray(msk[0])
     if not os.path.exists(args.save_dir):
@@ -101,6 +101,6 @@ def main():
 
     print('The output file has been saved to {}'.format(args.save_dir + 'mask.png'))
 
-    
+
 if __name__ == '__main__':
     main()
